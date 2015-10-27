c1236d90f64da0b25e39405c12bf1e806fbd3120
terence-uzamere <otuzamere@quinnipiac.edu>
2015-10-27 01:07:49 +0000

    Add about me page

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
   url(r'^$', Home.as_view(), name='home'),
   url(r'^question/create/$', ContactCreateView.as_view(), name='contact_form'),
   url(r'^success/$', Success.as_view(), name='success'),
+  url(r'^about_me/$', About_Me.as_view(), name='about_me'),
   url(r'^user/', include('registration.backends.simple.urls')),
   url(r'^user/', include('django.contrib.auth.urls')),
 )
